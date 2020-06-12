9fc9e56aa884685a97ee16d3256cf334dad2743b
Cory <cory.leach@gmail.com>
2020-06-11 22:49:42 -0400

    moving some stuff to script from yml

--- bin/before-install.sh
+++ bin/before-install.sh
@@ -3,6 +3,16 @@
 echo "Before Installing"
 ls
 
+curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/install.sh > ./install.sh
+curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/auth.sh > ./auth.sh
+curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/run-tests.sh > ./run-tests.sh
+curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/deauth.sh > ./deauth.sh
+
+chmod +x ./install.sh
+chmod +x ./auth.sh
+chmod +x ./run-tests.sh
+chmod +x ./deauth.sh
+
 #install JQ for parsing json list of unity versions
 brew install jq
 brew install node
