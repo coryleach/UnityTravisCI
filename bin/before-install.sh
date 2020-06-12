bd67f4aa17feea24feb4e7a444de0f3195ad4c7c
Cory <cory.leach@gmail.com>
2020-06-11 23:01:59 -0400

    Update before-install.sh

--- bin/before-install.sh
+++ bin/before-install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-echo "Before Installing"
-ls
+echo "Preparing to Install"
 
-curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/install.sh > ./install.sh
-curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/auth.sh > ./auth.sh
-curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/run-tests.sh > ./run-tests.sh
-curl -fsSL https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/deauth.sh > ./deauth.sh
+declare -a FILES=("install.sh"
+                  "auth.sh"
+                  "run-tests.sh"
+                  "deauth.sh")
 
-chmod +x ./install.sh
-chmod +x ./auth.sh
-chmod +x ./run-tests.sh
-chmod +x ./deauth.sh
+#Download Files
+for FILE in "${FILES[@]}"
+do
+   echo "Downlaoding ${FILE}"
+   curl -fsSL "https://raw.githubusercontent.com/coryleach/UnityTravisCI/master/bin/${FILE}" > "./${FILE}"
+   chmod +x "./${FILE}"
+done
 
 #install JQ for parsing json list of unity versions
 brew install jq
