dc2e3b29a2cd8485fd5139ba7eb9c8a4d455d792
Cory <cory.leach@gmail.com>
2020-06-11 23:53:34 -0400

    Update auth.sh

--- bin/auth.sh
+++ bin/auth.sh
@@ -2,8 +2,16 @@
 
 echo "Looking for Unity..."
 UNITY_INSTALLS=$(find /Applications/Unity -name "Unity.app")
+UNITY_INSTALL_LAST=$(head -1 <<< "${UNITY_INSTALLS}")
+
 echo $UNITY_INSTALLS
-UNITY_APP=$(grep "${UNITY_VERSION}" <<< "$UNITY_INSTALLS")
+
+if [ ! -z "${UNITY_VERSION}" ]; then
+  UNITY_APP_MATCHED=$(grep "${UNITY_VERSION}" <<< "$UNITY_INSTALLS")
+fi
+
+UNITY_APP=${UNITY_APP_MATCHED:-$UNITY_INSTALL_LAST}
+
 echo "found:" $UNITY_APP
 export UNITY_APP
 
