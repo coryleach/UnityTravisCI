b8b9712c8b23ab177cdfdae12f4cf34b27bba8a6
coryleach <cory.leach@gmail.com>
2020-06-15 15:05:33 -0400

    Update template. Fixing setup-project to not succeed the build too early.

--- bin/setup-project.sh
+++ bin/setup-project.sh
@@ -10,6 +10,7 @@ fi
 PACKAGE_FILE="$(pwd)/package.json"
 
 if [ ! -f "$PACKAGE_FILE" ]; then
+
     echo "Project is not a package."
 
     if [ ! -z "${UNITY_PROJECT_FOLDER}" ]; then
@@ -23,29 +24,31 @@ if [ ! -f "$PACKAGE_FILE" ]; then
     echo "Project Path: ${UNITY_PROJECT_PATH}"
     echo "If project path is incorrect make sure to correctly configure the UNITY_PROJECT_FOLDER environment variable"
     export UNITY_PROJECT_PATH
-    exit 0;
+
+else
+
+  echo "Test target is a package"
+  echo "Creating project for testing"
+  UNITY_PROJECT_PATH="${HOME}/TestProject"
+  export UNITY_PROJECT_PATH
+  echo "Project Path: ${UNITY_PROJECT_PATH}"
+
+  #Create Project
+  "${UNITY_APP}/Contents/MacOS/Unity" \
+      -batchmode \
+      -nographics \
+      -quit \
+      -logFile - \
+      -projectPath "${UNITY_PROJECT_PATH}"  || exit 1
+
+  #Modify packages to include our test package
+  echo "Adding package name to manifest.json"
+  MANIFEST_FILE="${UNITY_PROJECT_PATH}/Packages/manifest.json"
+  if [ ! -f "${MANIFEST_FILE}" ]; then
+    echo "Packages/manifest.json file not found in created project."
+  fi
+
+  PACKAGE_NAME=$(jq '.name' "${PACKAGE_FILE}")
+  jq ".dependencies += { \"${PACKAGE_NAME}\" : \"file:$(pwd)\" }" > "${MANIFEST_FILE}" || exit 1
+
 fi
-
-echo "Test target is a package"
-echo "Creating project for testing"
-UNITY_PROJECT_PATH="${HOME}/TestProject"
-export UNITY_PROJECT_PATH
-echo "Project Path: ${UNITY_PROJECT_PATH}"
-
-#Create Project
-"${UNITY_APP}/Contents/MacOS/Unity" \
-    -batchmode \
-    -nographics \
-    -quit \
-    -logFile - \
-    -projectPath "${UNITY_PROJECT_PATH}"  || exit 1
-
-#Modify packages to include our test package
-echo "Adding package name to manifest.json"
-MANIFEST_FILE="${UNITY_PROJECT_PATH}/Packages/manifest.json"
-if [ ! -f "${MANIFEST_FILE}" ]; then
-  echo "Packages/manifest.json file not found in created project."
-fi
-
-PACKAGE_NAME=$(jq '.name' "${PACKAGE_FILE}")
-jq ".dependencies += { \"${PACKAGE_NAME}\" : \"file:$(pwd)\" }" > "${MANIFEST_FILE}" || exit 1
