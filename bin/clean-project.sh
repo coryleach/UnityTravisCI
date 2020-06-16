ac24ca11d3138766b3f0382866eecf7722156d07
coryleach <cory.leach@gmail.com>
2020-06-16 00:17:22 -0400

    Update clean-project.sh

--- bin/clean-project.sh
+++ bin/clean-project.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #if we created a test project we delete it here
-PATH="${HOME}/TestProject"
+UNITY_PROJECT_CACHE="${HOME}/project_cache"
+PATH="${UNITY_PROJECT_CACHE}/TestProject"
 if [ -f $PATH ]; then
   echo "Removing test project"
   rm -rf $PATH
